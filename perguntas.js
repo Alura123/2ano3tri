60d6983a3dcf52eadc9c0b9a33e0b04fd446e30b
Alura123 <161342879+Alura123@users.noreply.github.com>
2024-09-23 10:42:25 -0300

    new

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Que livro é esse?',
+    'Uma princesa perdida que precisa retomar o trono e enfrentar inimigos..',
+    'Trono de VIdro.'
+)
+
+criaCartao(
+    'Que série é essa?',
+    'Investigação de casos.',
+    'CSI: Crime Scene Investigation.'
+)
+
+criaCartao(
+    'Que filme é esse?',
+    'Tem uma escola e um trio de amigos.',
+    'Harry Potter.'
+)
