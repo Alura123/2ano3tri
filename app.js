bbfc43f05bbaa1c5534cfad0f75df92bf32b32e2
Alura123 <161342879+Alura123@users.noreply.github.com>
2024-09-30 10:56:09 -0300

    mmmmmm

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ function criaCartao (categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
         cartao.className = 'cartao'
-cartao.innerHTML = `
-<div class="cartao__conteudo">
-<h3>$Que livro é esse?</h3>
-<div class="cartao__conteudo__pergunta">
-        <p>$Uma princesa perdida que precisa retomar o trono e enfrentar inimigos..</p>
-</div>
-<div class="cartao__conteudo__resposta">
-        <p>$Trono de VIdro.</p>
-</div>
-</div>
+        cartao.innerHTML = `
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+            <p>${resposta}</p>
+        </div>
+        </div>
 `
 let respostaEstaVisivel = false
 function viraCartao(){
